df581bb0f81048b5619242a1211ff7a2b033c6f9
Juraci Vieira <juraci.vieira@gmail.com>
2016-08-23 13:52:56 -0300

    Declaring the routes in the app

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  response.send('Hello world');
+});
+
 export default app;
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 import app from './app.js';
 
-app.get('/', (request, response) => {
-  response.send('Hello world');
-});
-
 app.listen(3000);
