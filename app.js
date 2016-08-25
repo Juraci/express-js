388b69ec7c4146426ae9027ab6c338aaaaa1ff48
Juraci Vieira <juraci.vieira@gmail.com>
2016-08-24 21:10:06 -0300

    Get block by name

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json('Hello world');
-});
+const blocks = {
+  'Fixed': 'Fastened securely in position',
+  'Movable': 'Capable of being moved',
+  'Rotating': 'Moving in a circle around its center'
+};
 
 app.route('/blocks').get((req, res) => {
-  let blocks = ['Fixed', 'Movable', 'Rotating'];
   let response;
+  let ablocks = [];
+
+  for(let key in blocks) {
+    ablocks.push(key);
+  }
 
   if(req.query.limit > 0) {
-    response = blocks.slice(0, req.query.limit);
+    response = ablocks.slice(0, req.query.limit);
   } else {
-    response = blocks;
+    response = ablocks;
   }
   res.json(response);
+});
+
+app.route('/blocks/:name').get((req, res) => {
+  let description = blocks[req.params.name];
+
+  if(!description) {
+    res.status(404).json('Block not found');
+  } else {
+    res.json(description);
+  }
 });
 
 export default app;
--- test/integration/blocks.js
+++ test/integration/blocks.js
@@ -1,27 +1,54 @@
-describe('GET /blocks', function() {
-  it('returns a list of blocks', function(done) {
-    request
-      .get('/blocks')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.have.length(3);
-        expect(res.body).to.include('Fixed');
-        expect(res.body).to.include('Movable');
-        expect(res.body).to.include('Rotating');
-        done(err);
-      });
-  });
+describe('Blocks resource', function() {
 
-  context('when passing "limit=1" as a query parameter', function() {
-    it('returns the first block only', function(done) {
+  describe('GET /blocks', function() {
+    it('returns a list of blocks', function(done) {
       request
-        .get('/blocks?limit=1')
+        .get('/blocks')
         .end((err, res) => {
           expect(res.status).to.equal(200);
-          expect(res.body).to.have.length(1);
+          expect(res.body).to.have.length(3);
           expect(res.body).to.include('Fixed');
+          expect(res.body).to.include('Movable');
+          expect(res.body).to.include('Rotating');
           done(err);
         });
+    });
+
+    context('when passing "limit=1" as a query parameter', function() {
+      it('returns the first block only', function(done) {
+        request
+          .get('/blocks?limit=1')
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.length(1);
+            expect(res.body).to.include('Fixed');
+            done(err);
+          });
+      });
+    });
+  });
+
+  describe('GET /blocks/:name', function() {
+    it('returns the block value', function(done) {
+      request
+        .get('/blocks/Fixed')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.equal('Fastened securely in position');
+          done(err);
+        });
+    });
+
+    context('when the name does not exist', function() {
+      it('returns 404 block not found', function(done) {
+        request
+          .get('/blocks/nonExistingBlock')
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            expect(res.body).to.equal('Block not found');
+            done(err);
+          });
+      });
     });
   });
 });
